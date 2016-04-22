fa4ba6080b75c049b080dc217709a12075b861e6
Naqi Syed <naqisyed@gmail.com>
2016-04-21 19:42:24 -0500

    Initial Commit

---
+++ ParallaxHeader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ParallaxHeader
+//
+//  Created by Naqi Syed on 4/21/16.
+//  Copyright © 2016 Naqi Syed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
